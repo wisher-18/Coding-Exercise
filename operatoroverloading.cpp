72c4aad793d6d4bf4f2ecab987615e066efe11b8
wisher18 <risingyouth005@gmail.com>
2022-09-22 01:04:13 +0530

    cpp practice

---
+++ operatoroverloading.cpp
@@ -0,0 +1,64 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+class Matrix
+{
+    public:
+    vector<vector<int>> a;
+    Matrix operator+(Matrix &virus){
+        Matrix mat;
+        for(int i =0; i< virus.a.size(); i++)
+        {
+            vector<int> b;
+            for(int j=0; j<virus.a[i].size(); j ++)
+            {
+                b.push_back(a[i][j]+ virus.a[i][j]);
+            }
+        mat.a.push_back(b);   
+        }   
+        return mat;
+    }    
+    
+    
+    
+};
+int main () {
+   int cases,k;
+   cin >> cases;
+   for(k=0;k<cases;k++) {
+      Matrix x;
+      Matrix y;
+      Matrix result;
+      int n,m,i,j;
+      cin >> n >> m;
+      for(i=0;i<n;i++) {
+         vector<int> b;
+         int num;
+         for(j=0;j<m;j++) {
+            cin >> num;
+            b.push_back(num);
+         }
+         x.a.push_back(b);
+      }
+      for(i=0;i<n;i++) {
+         vector<int> b;
+         int num;
+         for(j=0;j<m;j++) {
+            cin >> num;
+            b.push_back(num);
+         }
+         y.a.push_back(b);
+      }
+      result = x+y;
+      for(i=0;i<n;i++) {
+         for(j=0;j<m;j++) {
+            cout << result.a[i][j] << " ";
+         }
+         cout << endl;
+      }
+   }  
+   return 0;
+}
